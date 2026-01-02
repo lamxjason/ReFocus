e74a8d0530029c96377e1ef165b4308f21d6dae7
Jason Lam <lamjason92@gmail.com>
2026-01-02 14:46:48 +0700

    Initial Commit

---
+++ Re:Focus/Re_FocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  Re_FocusApp.swift
+//  Re:Focus
+//
+//  Created by Jason Lam on 2026-01-02.
+//
+
+import SwiftUI
+
+@main
+struct Re_FocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
